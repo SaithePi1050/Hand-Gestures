d381a6e94ce2b0c4711886ed90ded12fa3f26b1f
SaithePi <89380734+SaithePi1050@users.noreply.github.com>
2022-11-14 18:25:27 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -37,3 +37,51 @@ function speak()
     var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
     synth.speak(utterThis);
 }
+
+function check() 
+{
+    img = document.getElementById('capture_image');
+    classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results) 
+{
+    if (error)
+    {
+        console.error(error);
+    } else
+    {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if (results[0].label == "Amazing") 
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if (results[0].label == "Good Job") 
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if (results[0].label == "Peace") 
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+
+        if (results[1].label == "Amazing") 
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128076;";
+        }
+        if (results[1].label == "Good Job") 
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128077;";
+        }
+        if (results[1].label == "Peace") 
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#9996;";
+        }
+    }
+
+}
